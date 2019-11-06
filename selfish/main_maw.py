175de85ee3bf6ada991b989ced8551a0d064cdbf
Allan Jabri <ajabri@gmail.com>
2019-11-06 03:33:04 -0800

    implemented policy reloading + testing, now pushing logs too for easy visualization

--- selfish/main_maw.py
+++ selfish/main_maw.py
@@ -35,21 +35,26 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--exp-name", default="test_maw", type=str)
 parser.add_argument("--task-id", default="MaxHeightTask", type=str)
 parser.add_argument("--team-size", default=1, type=int)
-parser.add_argument("--time-limit", default=10, type=int)
+parser.add_argument("--time-limit", default=10., type=float)
+parser.add_argument("--test", default=False, action="store_true")
 
 args = parser.parse_args()
-args.exp_name = "%s_%s" % (args.exp_name, '-'.join([
-    "%s:%s" % (k,getattr(args, k)) for k in ['task_id', 'team_size']
-]))
+args.exp_name = "%s_%s" % (args.exp_name, '-'.join(
+    ["%s:%s" % (k, getattr(args, k)) for k in ['task_id', 'team_size']]))
 
 ##################### Env constructor #####################
 
+
 def make_env():
     # Load the 2-vs-2 soccer environment with episodes of 10 seconds:
-    dm_env = maw.load(team_size=1, time_limit=10., task_id='MaxHeightTask')
+    dm_env = maw.load(
+        team_size=args.team_size,
+        time_limit=args.time_limit,
+        task_id=args.task_id)
     #dm_env = dm_soccer.load(team_size=2, time_limit=10.)
     env = GymEnvWrapper(dmc2gym.DmControlWrapper('', '', env=dm_env))
     return env
+
 
 ##################### Visualization function #####################
 import maw.utils
@@ -59,27 +64,35 @@ arena = env.task.arena
 terrain = maw.pitch.make_terrain(arena._res, arena._size)
 
 # Get the part of state that we care about (pose)
-obs_indices = [(0, (0,0))]
-for k,v in env.observation_spec()[0].items():
+obs_indices = [(0, (0, 0))]
+for k, v in env.observation_spec()[0].items():
     print(k, v.shape)
-    obs_indices.append((k, (v.shape[-1], v.shape[-1] + obs_indices[-1][-1][-1])))
+    obs_indices.append((k, (v.shape[-1],
+                            v.shape[-1] + obs_indices[-1][-1][-1])))
 obs_indices = dict(obs_indices)
-idx = obs_indices['position']
+
 
 def vis_trajs(samples, itr):
     ''' MiniBatchRL runner will call this function, feeding it samples '''
-    poses = samples.env.observation[..., idx[1]:idx[1]+idx[0]-1]
-    return maw.utils.dump_traj_vis(
-        poses, terrain, arena._size, itr, outdir=get_log_dir("run_%s" % args.exp_name))
+    return
+    # idx = obs_indices['position']
+    # poses = samples.env.observation[..., idx[1]:idx[1] + idx[0] - 1]
+    # return maw.utils.dump_traj_vis(
+    #     poses,
+    #     terrain,
+    #     arena._size,
+    #     itr,
+    #     outdir=get_log_dir("run_%s" % args.exp_name))
+
 
 ##################### Parallelization stuff #####################
 
 affinity_code = encode_affinity(
-    n_cpu_core=8,
+    n_cpu_core=2,
     n_gpu=0,
     hyperthread_offset=2,
     n_socket=1,
-    cpu_per_run=8,
+    # cpu_per_run=8,
 )
 
 
@@ -94,8 +107,7 @@ def build_and_train(log_dir, run_ID, config_key):
         EnvCls=make_env,
         env_kwargs={},
         CollectorCls=CpuResetCollector,
-        **config["sampler"]
-    )
+        **config["sampler"])
     algo = PPO(optim_kwargs=config["optim"], **config["algo"])
     agent = MultiFfAgent(model_kwargs=config["model"], **config["agent"])
     runner = MinibatchRl(
@@ -107,8 +119,55 @@ def build_and_train(log_dir, run_ID, config_key):
         **config["runner"])
     name = config["env"]["id"]
 
-    with logger_context(log_dir, run_ID, name, config):
+    with logger_context(log_dir, run_ID, name, config, snapshot_mode='last'):
         runner.train()
 
 
-build_and_train('', args.exp_name, "ppo_1M_cpu")
+def build_and_test(model_path, config_key):
+    import dmc_wrapper
+    from dm_control import viewer
+    from rlpyt.utils.buffer import buffer_from_example, torchify_buffer, numpify_buffer
+
+    config = configs[config_key]
+
+    agent = MultiFfAgent(
+        model_kwargs=config["model"],
+        # initial_model_state_dict=torch.load(model_path),
+        **config["agent"])
+
+    dm_env = maw.load(
+        team_size=args.team_size,
+        time_limit=args.time_limit,
+        task_id=args.task_id)
+    env = GymEnvWrapper(dmc2gym.DmControlWrapper('', '', env=dm_env))
+
+    agent.initialize(env.spaces)
+    agent.reset()
+    agent.eval_mode(0)
+
+    prev_action = env.action_space.null_value()
+
+    def get_prev_action():
+        return prev_action
+
+    def policy(time_step):
+        obs = dmc_wrapper.convertObservation(time_step.observation)
+        reward = time_step.reward
+        reward = np.asarray(reward) if reward is not None else reward
+
+        obs_pyt, act_pyt, rew_pyt = torchify_buffer((obs, get_prev_action(),
+                                                     reward))
+        # obs_pyt, rew_pyt = torchify_buffer((obs, reward))
+
+        act_pyt, agent_info = agent.step(obs_pyt.float(), act_pyt, rew_pyt)
+        # prev_action = act_pyt
+
+        return act_pyt
+
+    viewer.launch(dm_env, policy=policy)
+
+
+if args.test:
+    build_and_test("", "ppo_1M_cpu")
+else:
+    build_and_train('', args.exp_name, "ppo_1M_cpu")
